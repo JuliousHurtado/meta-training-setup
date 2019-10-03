c1301010ade432f63455b7cd2e9af6c814e25cc2
Julio Hurtado <julious.hurtado@gmail.com>
2019-10-02 21:00:42 -0300

    Testing with two datasets

--- 2Datasets.py
+++ 2Datasets.py
@@ -10,19 +10,20 @@ from torch import optim
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 
-from data.datasets import full_omniglot
+from data.datasets.full_omniglot import FullOmniglot
 
 from copy import deepcopy
 
 import learn2learn as l2l
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def accuracy(predictions, targets):
     predictions = predictions.argmax(dim=1).view(targets.shape)
     return (predictions == targets).sum().float() / targets.size(0)
 
 
-def fast_adapt(adaptation_data, evaluation_data, learner, loss, adaptation_steps, device):
+def fast_adapt(adaptation_data, evaluation_data, learner, loss, adaptation_steps):
     for step in range(adaptation_steps):
         data = [d for d in adaptation_data]
         X = th.cat([d[0].unsqueeze(0) for d in data], dim=0).to(device)
@@ -50,12 +51,11 @@ def getDatasets(dataset, ways):
                                 transform = transforms.Compose([
                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                 ]))
-            print(dataset[14][0].size())
-            print(dataset[14][0].sum())
+
             dataset = l2l.data.MetaDataset(dataset)
             generators[mode] = l2l.data.TaskGenerator(dataset=dataset, ways=ways, tasks=tasks)
     else:
-        omniglot = full_omniglot.FullOmniglot(root='./data/data',
+        omniglot = FullOmniglot(root='./data/data',
                                                 transform=transforms.Compose([
                                                     l2l.vision.transforms.RandomDiscreteRotation(
                                                         [0.0, 90.0, 180.0, 270.0]),
@@ -64,8 +64,7 @@ def getDatasets(dataset, ways):
                                                     lambda x: 1.0 - x,
                                                 ]),
                                                 download=False, to_color = True)
-        print(omniglot[12][0].size())
-        print(omniglot[12][0].sum())
+
         omniglot = l2l.data.MetaDataset(omniglot)
         classes = list(range(1623))
         random.shuffle(classes)
@@ -89,19 +88,17 @@ def main(
         shots=1,
         meta_lr=0.003,
         fast_lr=0.5,
-        meta_batch_size=32,
+        meta_batch_size=4,
         adaptation_steps=1,
-        num_iterations=60000,
+        num_iterations=6,
         cuda=True,
         seed=42,
 ):
     random.seed(seed)
     np.random.seed(seed)
     th.manual_seed(seed)
-    device = th.device('cpu')
     if cuda:
         th.cuda.manual_seed(seed)
-        device = th.device('cuda')
 
     # Create Datasets
     generators = {'mini-imagenet': None, 'omniglot': None}
@@ -110,78 +107,101 @@ def main(
     generators['omniglot'] = getDatasets('omniglot', ways)
     
     # Create model
-    # model = l2l.vision.models.OmniglotCNN(ways)
-    # model.to(device)
-    # maml = l2l.algorithms.MAML(model, lr=fast_lr, first_order=False)
-    # opt = optim.Adam(maml.parameters(), meta_lr)
-    # loss = nn.CrossEntropyLoss(size_average=True, reduction='mean')
+    model = l2l.vision.models.OmniglotCNN(ways)
+    model.to(device)
+    maml = l2l.algorithms.MAML(model, lr=fast_lr, first_order=False)
+    opt = optim.Adam(maml.parameters(), meta_lr)
+    loss = nn.CrossEntropyLoss(size_average=True, reduction='mean')
 
-    # for iteration in range(num_iterations):
-    #     opt.zero_grad()
-    #     meta_train_error = 0.0
-    #     meta_train_accuracy = 0.0
-    #     meta_valid_error = 0.0
-    #     meta_valid_accuracy = 0.0
-    #     meta_test_error = 0.0
-    #     meta_test_accuracy = 0.0
-    #     for task in range(meta_batch_size):
-    #         # Compute meta-training loss
-    #         learner = maml.clone()
-    #         adaptation_data = train_generator.sample(shots=shots)
-    #         evaluation_data = train_generator.sample(shots=shots,
-    #                                                  task=adaptation_data.sampled_task)
-    #         evaluation_error, evaluation_accuracy = fast_adapt(adaptation_data,
-    #                                                            evaluation_data,
-    #                                                            learner,
-    #                                                            loss,
-    #                                                            adaptation_steps,
-    #                                                            device)
-    #         evaluation_error.backward()
-    #         meta_train_error += evaluation_error.item()
-    #         meta_train_accuracy += evaluation_accuracy.item()
+    results = {
+        'train_acc': [],
+        'train_loss': [],
+        'val_acc': [],
+        'val_loss': [],
+        'test_acc': [],
+        'test_loss': [],
 
-    #         # Compute meta-validation loss
-    #         learner = maml.clone()
-    #         adaptation_data = valid_generator.sample(shots=shots)
-    #         evaluation_data = valid_generator.sample(shots=shots,
-    #                                                  task=adaptation_data.sampled_task)
-    #         evaluation_error, evaluation_accuracy = fast_adapt(adaptation_data,
-    #                                                            evaluation_data,
-    #                                                            learner,
-    #                                                            loss,
-    #                                                            adaptation_steps,
-    #                                                            device)
-    #         meta_valid_error += evaluation_error.item()
-    #         meta_valid_accuracy += evaluation_accuracy.item()
+    }
+    for dataset in ['mini-imagenet', 'omniglot']:
+        train_generator = generators[dataset][0]
+        valid_generator = generators[dataset][1]
+        test_generator = generators[dataset][2]
+        
+        for iteration in range(num_iterations):
+            opt.zero_grad()
+            meta_train_error = 0.0
+            meta_train_accuracy = 0.0
+            meta_valid_error = 0.0
+            meta_valid_accuracy = 0.0
 
-    #         # Compute meta-testing loss
-    #         learner = maml.clone()
-    #         adaptation_data = test_generator.sample(shots=shots)
-    #         evaluation_data = test_generator.sample(shots=shots,
-    #                                                 task=adaptation_data.sampled_task)
-    #         evaluation_error, evaluation_accuracy = fast_adapt(adaptation_data,
-    #                                                            evaluation_data,
-    #                                                            learner,
-    #                                                            loss,
-    #                                                            adaptation_steps,
-    #                                                            device)
-    #         meta_test_error += evaluation_error.item()
-    #         meta_test_accuracy += evaluation_accuracy.item()
+            for task in range(meta_batch_size):
+                # Compute meta-training loss
+                learner = maml.clone()
+                adaptation_data = train_generator.sample(shots=shots)
+                evaluation_data = train_generator.sample(shots=shots,
+                                                         task=adaptation_data.sampled_task)
+                evaluation_error, evaluation_accuracy = fast_adapt(adaptation_data,
+                                                                   evaluation_data,
+                                                                   learner,
+                                                                   loss,
+                                                                   adaptation_steps,
+                                                                   device)
+                evaluation_error.backward()
+                meta_train_error += evaluation_error.item()
+                meta_train_accuracy += evaluation_accuracy.item()
 
-    #     # Print some metrics
-    #     print('\n')
-    #     print('Iteration', iteration)
-    #     print('Meta Train Error', meta_train_error / meta_batch_size)
-    #     print('Meta Train Accuracy', meta_train_accuracy / meta_batch_size)
-    #     print('Meta Valid Error', meta_valid_error / meta_batch_size)
-    #     print('Meta Valid Accuracy', meta_valid_accuracy / meta_batch_size)
-    #     print('Meta Test Error', meta_test_error / meta_batch_size)
-    #     print('Meta Test Accuracy', meta_test_accuracy / meta_batch_size)
+                # Compute meta-validation loss
+                learner = maml.clone()
+                adaptation_data = valid_generator.sample(shots=shots)
+                evaluation_data = valid_generator.sample(shots=shots,
+                                                         task=adaptation_data.sampled_task)
+                evaluation_error, evaluation_accuracy = fast_adapt(adaptation_data,
+                                                                   evaluation_data,
+                                                                   learner,
+                                                                   loss,
+                                                                   adaptation_steps,
+                                                                   device)
+                meta_valid_error += evaluation_error.item()
+                meta_valid_accuracy += evaluation_accuracy.item()
 
-    #     # Average the accumulated gradients and optimize
-    #     for p in maml.parameters():
-    #         p.grad.data.mul_(1.0 / meta_batch_size)
-    #     opt.step()
+            err = []
+            acc = []
+            for dataset2 in ['mini-imagenet', 'omniglot']:
+                test_generator = generators[dataset2][2]
+                # Compute meta-testing loss
+                learner = maml.clone()
+                adaptation_data = test_generator.sample(shots=shots)
+                evaluation_data = test_generator.sample(shots=shots,
+                                                            task=adaptation_data.sampled_task)
+                evaluation_error, evaluation_accuracy = fast_adapt(adaptation_data,
+                                                                       evaluation_data,
+                                                                       learner,
+                                                                       loss,
+                                                                       adaptation_steps,
+                                                                       device)
+                err.append(evaluation_error.item())
+                acc.append(evaluation_accuracy.item())
+
+            # Print some metrics
+            print('\n')
+            print('Iteration', iteration)
+            print('Meta Train Error', meta_train_error / meta_batch_size)
+            print('Meta Train Accuracy', meta_train_accuracy / meta_batch_size)
+            results['train_loss'].append(meta_train_error / meta_batch_size)
+            results['train_acc'].append(meta_train_accuracy / meta_batch_size)
+
+            print('Meta Valid Error', meta_valid_error / meta_batch_size)
+            print('Meta Valid Accuracy', meta_valid_accuracy / meta_batch_size)
+            results['val_loss'].append(meta_valid_error / meta_batch_size)
+            results['val_acc'].append(meta_valid_accuracy / meta_batch_size)
+
+            results['test_loss'].append(err)
+            results['test_acc'].append(acc)
+
+            # Average the accumulated gradients and optimize
+            for p in maml.parameters():
+                p.grad.data.mul_(1.0 / meta_batch_size)
+            opt.step()
 
 if __name__ == '__main__':
     main()
