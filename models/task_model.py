Fix problem with the gradient of task convolutions

models/task_model.py
@@ -2,6 +2,7 @@ import copy
 
 import torch
 from torch import nn
+from torch.nn import functional as F
 
 import torchvision.models as models
 from learn2learn.vision.models import MiniImagenetCNN
@@ -12,6 +13,7 @@ class TaskEspecific(nn.Module):
 
         self.features = self.getExtractor()
         self.filters = filters
+        self.grads = {}
 
         mlp = []
         for elem in filters:
@@ -28,6 +30,12 @@ class TaskEspecific(nn.Module):
             p.requires_grad = False
 
         return extractor
+
+    # def saveGrad(self, name):
+    #     def hook(grad):
+    #         #print(grad)
+    #         self.grads[name] = grad
+    #     return hook
 
     def forward(self, x):
         x = self.features(x).view(x.size(0), -1)
@@ -77,6 +85,7 @@ class TaskModel(nn.Module):
 
                     selected_filters[count] = { 
                                 'index': torch.tensor(index[:int(sizes[0]*self.p_filter)]).to(self.device),
+                                'no_index': torch.tensor(index[int(sizes[0]*self.p_filter):]).to(self.device),
                                 'n_filters': int(sizes[0]*self.p_filter),
                                 'sizes': sizes}
                     p.weight[selected_filters[count]['index']].mul(0)
@@ -85,16 +94,39 @@ class TaskModel(nn.Module):
 
         return selected_filters
 
-    def setFilters(self, f):
-        count = 0
-        for elem in self.meta_model.base:
-            for p in elem.children():
-                if isinstance(p, torch.nn.Conv2d):
-                    p.weight[self.task_model.filters[count]['index']].add(f[count]) 
-                    count += 1
+    # def setFilters(self, f):
+    #     count = 0
+    #     for elem in self.meta_model.base:
+    #         for p in elem.children():
+    #             if isinstance(p, torch.nn.Conv2d):
+    #                 p.weight[self.task_model.filters[count]['index']].add(f[count])
+    #                 count += 1
 
-    def forward(self, x, y):
-        if self.training:
+    # def printGradConv(self):
+    #     count = 0
+    #     for elem in self.meta_model.base:
+    #         for p in elem.children():
+    #             if isinstance(p, torch.nn.Conv2d):
+    #                 print(p.weight[self.task_model.filters[count]['index']].grad)
+    #                 count += 1
+
+    def getTaskParameters(self):
+        params = []
+        for p in self.task_model.mlp.parameters():
+            params.append(p)
+
+        for p in self.meta_model.linear.parameters():
+            params.append(p)
+
+        return params
+
+    # def saveGradTask(self):
+    #     for i, elem in enumerate(self.task_model.filters):
+    #         self.task_model.mlp[i].weight.register_hook(print)
+
+    def alternative_forward(self, x, y):
+        x.requires_grad = True
+        if False:#self.training:
             p = int(x.size(0)/2)
             x1 = x[:p]
             y1 = y[:p]
@@ -105,8 +137,23 @@ class TaskModel(nn.Module):
             x2 = copy.deepcopy(x)
             y2 = y
 
-        f = self.task_model(x1)
+        f = self.task_model(x)
         self.setFilters(f)
-        out = self.meta_model(x2)
+        out = self.meta_model(x)
 
         return out, y2
+
+    def forward(self, x, y):
+        f = self.task_model(x)
+        for i, elem in enumerate(self.meta_model.base):
+            x1 = F.conv2d(x, elem.conv.weight[self.task_model.filters[i]['no_index']], None, elem.conv.stride, elem.conv.padding, elem.conv.dilation, elem.conv.groups)
+            x2 = F.conv2d(x, f[i], None, elem.conv.stride, elem.conv.padding, elem.conv.dilation, elem.conv.groups)
+            x = torch.cat([x1,x2], dim=1)
+            
+            x = elem.normalize(x)
+            x = elem.relu(x)
+            x = elem.max_pool(x)
+
+        out = self.meta_model.linear(x.view(-1, 25 * 32))
+        return out, y
+        

task_train.py
@@ -28,7 +28,7 @@ def getDataset(name_dataset):
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
-    bs = 4
+    bs = 64
     if name_dataset == 'SVHN':
         dataset = SVHN('./data/', split='train', transform=transform_data, download=True)        
         generators['train'] = torch.utils.data.DataLoader(dataset, batch_size=bs)
@@ -71,7 +71,7 @@ def addResults(model, data_generators, results, iteration, train_error, train_ac
     results['test_acc'].append(test_accuracy)
 
 def main(model, data_generators, device, lr=0.003, args=None):
-    opt = optim.Adam(model.task_model.parameters(), lr)
+    opt = optim.Adam(model.getTaskParameters(), lr)
     loss = nn.CrossEntropyLoss(reduction='mean')
 
     results = {
@@ -108,7 +108,7 @@ if __name__ == '__main__':
 
     device = torch.device("cuda" if use_cuda else "cpu")
     
-    model = TaskModel(os.path.join('./results', args.load_model), 0.5, device).to(device)
+    model = TaskModel(os.path.join('./results', args.load_model), args['percentage_new_filter'], device).to(device)
     model.setLinear(0, 10)
     data_generators = getDataset(args.dataset)
 

utils.py
@@ -21,68 +21,49 @@ def str2bool(v):
 def getArguments():
     parser = argparse.ArgumentParser(description='Learn2Learn MNIST Example')
 
-    parser.add_argument('--ways', type=int, default=5, metavar='N',
-                        help='number of ways (default: 5)')
-    parser.add_argument('--shots', type=int, default=5, metavar='N',
-                        help='number of shots (default: 5)')
-    parser.add_argument('--hidden-size', type=int, default=32, metavar='N',
-                        help='number of shots (default: 5)')
-    parser.add_argument('-tps', '--tasks-per-step', type=int, default=8, metavar='N',
-                        help='tasks per step (default: 8)')
-    parser.add_argument('-fas', '--fast-adaption-steps', type=int, default=5, metavar='N',
-                        help='steps per fast adaption (default: 5)')
 
-    parser.add_argument('--iterations', type=int, default=15000, metavar='N',
-                        help='number of iterations (default: 15000)')
-    parser.add_argument('--weight_decay', type=float, default=0.0, metavar='N',
-                        help='weigth decay of Adam')
+    #--------------------------------Meta Train-----------------------------------------#
+    parser.add_argument('--ways', type=int, default=5)
+    parser.add_argument('--shots', type=int, default=5)
+    parser.add_argument('--fast-lr', type=float, default=0.5)
+    parser.add_argument('-tps', '--tasks-per-step', type=int, default=8)
+    parser.add_argument('-fas', '--fast-adaption-steps', type=int, default=5)
+    parser.add_argument('--first-order', type=str2bool, default=False)
 
+    #--------------------------------Model----------------------------------------------#
+    parser.add_argument('--hidden-size', type=int, default=32)
+    parser.add_argument('--percentage-new-filter', type=float, default=0.2)
+
+
+    #--------------------------------Training-------------------------------------------#
+    parser.add_argument('--iterations', type=int, default=80)
+    parser.add_argument('--weight_decay', type=float, default=0.0)
     parser.add_argument('--dataset', type=str, default='Omniglot', metavar='C',
                         help='[Omniglot, MiniImagenet, cifar10, SVHN]')
-    parser.add_argument('--input-channel', type=int, default=1, metavar='C',
-                        help='1 for using black and white image and 3 for RGB')
+    parser.add_argument('--lr', type=float, default=0.003)
+    
+    
 
-    parser.add_argument('--lr', type=float, default=0.003, metavar='LR',
-                        help='learning rate (default: 0.003)')
-    parser.add_argument('--fast-lr', type=float, default=0.5, metavar='LR',
-                        help='learning rate for MAML (default: 0.5)')
-    parser.add_argument('--first-order', type=str2bool, default=False, metavar='LR',
-                        help='Using First order MAML')
-    parser.add_argument('--freeze-layer', nargs='+', type=int, metavar='LR',
-                        help='List of frozen layers')
-
+    #------------------------------CF-Regularization-------------------------------------#
     parser.add_argument('--use-ewc', type=str2bool, default=False,
                         help='Use EWC')
-    parser.add_argument('--ewc-importance', type=float, default=100, metavar='LR',
-                        help='learning rate for MAML (default: 0.5)')
+    parser.add_argument('--ewc-importance', type=float, default=100)
 
-    parser.add_argument('--seed', type=int, default=42, metavar='S',
-                        help='random seed (default: 42)')
 
-    parser.add_argument('--algorithm', type=str, default='MAML',
-                        help='[MAML, ANIL, FT]')
-    parser.add_argument('--remap-label', type=str2bool, default=True, metavar='LR',
-                        help='Used in original MAML, to remap Label to amount of ways')
+    #--------------------------------Extra----------------------------------------------#
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--save-model', type=str2bool, default=True)
+    parser.add_argument('--load-model', type=str, default='./results/temp.pth')
+    parser.add_argument('--load-head', type=str, default='')
 
-    parser.add_argument('--filter-reg', type=str2bool, default=False,
-                        help='Using or not sparse-group regularization in conv filter (default False)')
-    parser.add_argument('--cost-theta', type=float, default=0.01,
-                        help='cost value of filter reg (default 0.01)')
-    parser.add_argument('--linear-reg', type=str2bool, default=False,
-                        help='Using or not sparse-group regularization in linear layer (default False)')
-    parser.add_argument('--cost-omega', type=float, default=0.01,
-                        help='cost value of linear reg (default 0.01)')
-    parser.add_argument('--sparse-reg', type=str2bool, default=False,
-                        help='Using or not sparse-group regularization in conv filter (default False)')
 
-    parser.add_argument('--save-model', type=str2bool, default=True, metavar='LR',
-                        help='save model (default True)')
-    parser.add_argument('--load-model', type=str, default='', metavar='LR',
-                        help='Model to Load, ./results/models/name_file')
-    parser.add_argument('--load-head', type=str, default='', metavar='LR',
-                        help='Head of the modelo Load from , ./results/models/name_file')
-    parser.add_argument('--init-ways', type=int, default=200, metavar='N',
-                        help='number of ways (default: 5)')
+    #---------------------------------Regularization-------------------------------------#
+    parser.add_argument('--filter-reg', type=str2bool, default=False)
+    parser.add_argument('--sparse-reg', type=str2bool, default=False)
+    parser.add_argument('--cost-theta', type=float, default=0.01)
+    parser.add_argument('--linear-reg', type=str2bool, default=False)
+    parser.add_argument('--cost-omega', type=float, default=0.01)
+
 
     return parser
 
@@ -211,7 +192,7 @@ def train_normal(data_loader, learner, loss, optimizer, regs, device, ewc = None
         running_loss += l.item()
         running_corrects += torch.sum(preds == labels.data)
 
-    return running_loss / len(data_loader), running_corrects / len(data_loader)
+    return running_loss / len(data_loader), running_corrects.item() / len(data_loader)
 
 def test_normal(model, data_loader, device):
     model.eval()