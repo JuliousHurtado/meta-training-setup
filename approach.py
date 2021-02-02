d272506765d0b22b5762771b47aac0f45c35adcf
Julio Hurtado <julious.hurtado@gmail.com>
2021-02-02 17:03:07 -0300

    Fix hyper parameters in optimizers

--- approach.py
+++ approach.py
@@ -331,7 +331,7 @@ def training_procedure(args, net, task_id, dataloader, criterion, device, memory
             params.append(p)
     for e in range(args.meta_epochs):
         if args.use_meta:
-            opti_shared = optim.SGD(params, args.lr_meta*0.1, weight_decay=0.4) # *0.1, weight_decay=0.9 
+            opti_shared = optim.SGD(params, args.lr_meta*0.1, weight_decay=0.8) # *0.1, weight_decay=0.9 
             meta_acc, meta_loss = meta_training(args, net, dataloader['train'], task_id, opti_shared, criterion, device, memory)
         else:
             opti_shared = optim.SGD(params, args.lr_meta) # 
