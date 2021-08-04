86f466a4fcd7a660301c7cb6c299a16725133ec9
Julio Hurtado <julious.hurtado@gmail.com>
2021-08-04 15:36:23 -0400

    Change gamma = 1 - gamma

--- approach.py
+++ approach.py
@@ -154,7 +154,7 @@ def meta_training(args, net, loader, task_id, opti_shared, criterion, device):
     
     for i, g in enumerate(grads_acc['grads']):
         for n in g:
-            save_grads[n] += g[n].to(device)*1/(args.mini_tasks) # weights[i]
+            save_grads[n] += g[n].to(device)*(1-weights[i])/(args.mini_tasks) # 
 
     set_grads(net, save_grads, task_id)
 
